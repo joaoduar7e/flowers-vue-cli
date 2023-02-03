451900c633abe95c79d77c2c43a03da5b70738d8
joaoduar7e <joao.duarte@edu.unipar.br>
2023-02-03 20:14:55 -0300

    ajuste

--- src/components/View/View.vue
+++ src/components/View/View.vue
@@ -1,5 +1,5 @@
 <template>
-  <body class="fadeIn">
+  <body>
     <div class="div-img-principal">
       <img class="img-principal" :src="flower.first_img" alt="" />
     </div>
@@ -81,44 +81,8 @@ body {
   font-family: "Quicksand";
 }
 
-.fadeIn {
-  -webkit-animation: fadeIn 3s ease-in-out;
-  -moz-animation: fadeIn 3s ease-in-out;
-  -o-animation: fadeIn 3s ease-in-out;
-  animation: fadeIn 3s ease-in-out;
-}
-
-@-webkit-keyframes fadeIn {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-@-moz-keyframes fadeIn {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-@-o-keyframes fadeIn {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-@keyframes fadeIn {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
+.media-element > img {
+  border-radius: 16px;
 }
 
 .all-infos {
